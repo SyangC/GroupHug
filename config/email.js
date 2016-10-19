185a67058bfa6b3c2bc308a0397e0948aa24355e
Shu-yang Chia <shuyangchia@hotmail.co.uk>
2016-10-19 17:54:32 +0100

    automailer working for registration. Will add more templates

--- config/email.js
+++ config/email.js
@@ -6,26 +6,25 @@ var smtpConfig = {
   secure: true,
   auth: {
     user: process.env.GMAIL_ID,
-    pass: process.env.GMAIL_PASSWORD,
+    pass: process.env.GMAIL_PASSWORD
   }
 };
 
-var transporter = nodemailer.createTransport(smtpConfig)
+var transporter = nodemailer.createTransport(smtpConfig);
 
 var registerTemplate = transporter.templateSender({
   subject: '{{firstName }}, Welcome to GroupHug!',
   text: "This is the text version from GroupHug. Hi {{firstName}}, thank you for registering with us.",
   html: "<head><style>.body{background-color: #E4DFDA}h1{color: #4281a4}</style></head><body class='body'><h1>GroupHug</h1><br><h3>Hi {{firstName}}, thank you for registering with us.</h3></body>"
 }, { from: process.env.GMAIL_ID });
-  function sendRegisterTemplate(order) { 
+  function sendRegisterTemplate(user) { 
 
-  email = order.user.email;
-  date_in = order.createdAt;
+  email = user.email;
   // Error handling
+  firstName_in = user.firstName;
   if(firstName_in == undefined){firstName_in = ""};
-  lastName_in = order.user.lastName;
+  lastName_in = user.lastName;
   if(lastName_in  == undefined){lastName_in = ""};
-  buildingNumber_in = order.user.buildingNumber;
 
   registerTemplate({
     to: email
@@ -34,7 +33,7 @@ var registerTemplate = transporter.templateSender({
     lastName: lastName_in,
   }, function(err, info){
     if(err){
-     console.log('Error', err);
+      console.log('Error', err);
     }else{
       console.log('Yeah, email sent!');
     }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 var jwt = require("jsonwebtoken");
 var User = require("../models/user");
 var secret = require("../config/tokens").secret;
+var email = require("../config/email");
 
 function login(req, res) {
   User.findOne({ email: req.body.email }, function(err, user) {
@@ -25,7 +26,7 @@ function register(req, res) {
 
     var payload = { _id: user._id, username: user.username };
     var token = jwt.sign(payload, secret, { expiresIn: 60*60*24 });
-
+    email.sendRegisterTemplate(user);
     return res.status(200).json({
       message: "Thanks for registering!",
       token: token
