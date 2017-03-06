bd6864c28a2505020c71e5f56dabdb69c17a5f6f
JulesJam <julian.wyatt@1xdconsulting.co.uk>
2017-03-06 19:59:30 +0000

    trying to redisable user register email

--- controllers/auth.js
+++ controllers/auth.js
@@ -31,19 +31,19 @@ function register(req, res) {
     var payload = { _id: user._id, username: user.username };
     var token = jwt.sign(payload, secret, { expiresIn: 60*60*24 });
     var date = new Date();
-    EmailTemplate.findOne({'name': 'Registration'})
-      .then(function(registrationEmail) {
-        var newDate = date.setSeconds(date.getSeconds() + registrationEmail.delay);
-        email.sendRegisterTemplate(user);
-        var j = schedule.scheduleJob(newDate, function(){
-          email.sendRegisterTemplate(user);
-          console.log('This works? Hopefully');
-        });
+    // EmailTemplate.findOne({'name': 'Registration'})
+    //   .then(function(registrationEmail) {
+    //     var newDate = date.setSeconds(date.getSeconds() + registrationEmail.delay);
+    //     email.sendRegisterTemplate(user);
+    //     var j = schedule.scheduleJob(newDate, function(){
+    //       email.sendRegisterTemplate(user);
+    //       console.log('This works? Hopefully');
+    //     });
         return res.status(200).json({
           message: "Thanks for registering!",
           token: token
         })        
-      })
+      // })
       .catch(function(err) {
         console.log(err);
         res.status(500).json(err);
